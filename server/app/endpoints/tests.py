48eca9f7b8236333e5352a6f597cec30216e40ea
djamik <jamal.abakaroff@gmail.com>
2024-12-05 05:44:42 +0300

    "Я не исправляю баги. Я устраняю тех, кто их создал." - Джейсон Стетхэм

--- server/app/endpoints/tests.py
+++ server/app/endpoints/tests.py
@@ -8,6 +8,7 @@ async def read_item(item_id: str, db=Depends(get_db)):
     cursor, conn = db
     cursor.execute("SELECT * FROM items WHERE find_val = %s", (item_id,))
     item = cursor.fetchone()
+
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
