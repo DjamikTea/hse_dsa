4032b26ccfdffe0349f434cc4a88ba970c89d74d
djamik <jamal.abakaroff@gmail.com>
2024-12-09 03:49:22 +0300

    “Когда я делаю деплой, сервера встают навытяжку.” - Джейсон Стетхэм

---
+++ server/crypto/hsecrypto/__init__.py
@@ -0,0 +1,4 @@
+from .dsa.gost_dsa import GostDSA
+from .lib.curve import EllipticCurve, Point
+
+__all__ = ["GostDSA", "EllipticCurve", "Point"]
---
+++ server/crypto/tests/test_dsa.py
@@ -0,0 +1,24 @@
+import pytest
+
+import hsecrypto
+
+
+@pytest.fixture
+def dsa():
+    return hsecrypto.GostDSA()
+
+
+@pytest.fixture
+def keys(dsa: hsecrypto.GostDSA):
+
+    private_key, public_key = dsa.generate_key_pair()
+    return private_key, public_key
+
+
+def test_sign_and_verify(dsa: hsecrypto.GostDSA, keys: tuple[str, str]):
+    private_key, public_key = keys
+    signature = dsa.sign(b"test", private_key=private_key)
+
+    result = dsa.check(signature=signature, message=b"test", public_key=public_key)
+
+    assert result is True
